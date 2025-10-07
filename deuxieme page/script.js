11fb5dde93e27e058d886a53d6430aa5fddeed10
Omen46-ui <louisgerard2000@free.fr>
2025-10-07 21:19:06 +0200

    Add files via upload

---
+++ deuxieme page/script.js
@@ -0,0 +1,20 @@
+function modifierlien()
+{
+	var wiki = document.querySelector(
+  "header.wiki input[href='https://fr.wikipedia.org/wiki/Wikipédia:Accueil_principal']",
+);
+
+}
+
+document.getElementById("texte", function){
+	if (texte != "oui" || texte != "non")
+	{
+		texte == "il faut mettre oui ou non";
+	}
+}
+
+
+
+var wiki = document.querySelector(
+  "header.wiki input[href='https://fr.wikipedia.org/wiki/Wikipédia:Accueil_principal']",
+);
